2631db8f989c5b1881a339fa9c6cd8e7263ac4ca
saurabhkemekar <50518930+saurabhkemekar@users.noreply.github.com>
2019-12-21 18:31:00 +0530

    Update cylinder_panorama.py

--- cylinder_panorama.py
+++ cylinder_panorama.py
@@ -1,62 +1,23 @@
 import cv2
 import numpy as np
 import os
-import math
 #os.chdir('/home/saurabh/PANORAMA/3')
-def arrange_points(points):
-    number = np.array([a[0] for a in points])
-    x_cen = 0
-    y_cen = 0
-    for [x, y] in number:
-        x_cen = x_cen + x
-        y_cen = y_cen + y
-    x_cen = x_cen // 4
-    y_cen = y_cen // 4
-    sorted_x = np.array([[[0, 0]], [[0, 0]], [[0, 0]], [[0, 0]]])
-    for [x, y] in number:
-        if x <= x_cen and y <= y_cen:
-            sorted_x[0][0] = np.array([x, y])
-        if x <= x_cen and y >= y_cen:
-            sorted_x[1][0] = np.array([x, y])
-        if x >= x_cen and y >= y_cen:
-            sorted_x[2][0] = np.array([x, y])
-        if x >= x_cen and y <= y_cen:
-            sorted_x[3][0] = np.array([x, y])
-    points = np.array(sorted_x)
-    return  points
 
-def required_img(img):
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    ret,thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
-    image,contour,h = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.imshow('thresh',thresh)
-    try:
-        cnt =contour[0]
-        epsilon = 0.1*cv2.arcLength(cnt,True)
-        approx = cv2.approxPolyDP(cnt,epsilon,True)
-        approx = arrange_points(approx)
-        x,y,w,h = cv2.boundingRect(cnt)
-        if approx[2][0][0] < approx[3][0][0]:
-            img = img[y:y+h,x:x+approx[2][0][0]]
-        else:
-            img = img[y:y + h, x:x + approx[3][0][0]]
-    except Exception:
-        return img
-    return  img
-
-noi = 2 # number of images to be stich
+noi = 4 # number of images to be stich
 img1 = cv2.imread('1.jpeg')
 img1 = cv2.resize(img1, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
 hieght  = img1.shape[0] + 50
 k = np.array([[583.61969059, 0, 327.46517597], [0, 582.90936082, 251.76312815], [0, 0, 1]])
-#img1 = cylindricalWarpImage(img1,k)
 img3 = img1.copy()
+img1,A = cylindrical_warp(img1,k) 
+xyz.append(A)
+
 for i in range(1,noi):
-        img3 = required_img(img3)
         img1 = img3
         img2 = cv2.imread(str(i + 1) + '.jpeg')
         img2 = cv2.resize(img2, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+        img2,A = cylindrical_warp(img2,k)
+        xyz.append(A)
         gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
         gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
         sift = cv2.xfeatures2d.SIFT_create()
@@ -78,9 +39,14 @@ for i in range(1,noi):
         print('HOMOGRAPHY',H)
         H = np.linalg.inv(H)
         img3 = cv2.warpPerspective(img2,H,(gray1.shape[1]+gray2.shape[1],hieght))
-        img3[0:gray1.shape[0],0:gray1.shape[1]] = img1
+        temp = np.zeros_like(img3)
+        col = img3.shape[1]-canvas.shape[1]
+        temp = cv2.resize(temp,(col,img3.shape[0]),interpolation = cv2.INTER_CUBIC)
+        canvas = np.hstack((canvas,temp))
+        canvas = pano(canvas,img3)
+        cv2.imshow('canvas',canvas)
+        cv2.waitKey(0)
 
-img3 = required_img(img3)
 cv2.imshow('warped_image',img3)
 cv2.imwrite('img.jpg',img3)
 cv2.waitKey(0)
