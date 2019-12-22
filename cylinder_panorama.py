584bd8db51fa106095234d747458086bc6f54bf6
saurabhkemekar <50518930+saurabhkemekar@users.noreply.github.com>
2019-12-22 11:49:22 +0530

    Update cylinder_panorama.py

--- cylinder_panorama.py
+++ cylinder_panorama.py
@@ -3,6 +3,22 @@ import numpy as np
 import os
 #os.chdir('/home/saurabh/PANORAMA/3')
 
+def cylinder_coordinates(img,K):
+    foc_len = (K[0][0] +K[1][1])/2
+    cylinder = np.zeros_like(img)
+    temp = np.mgrid[0:img.shape[1],0:img.shape[0]]
+    x,y = temp[0],temp[1]
+    theta= (x- K[0][2])/foc_len # angle theta
+    h = (y-K[1][2])/foc_len # height
+    p = np.array([np.sin(theta),h,np.cos(theta)])
+    p = p.T
+    p = p.reshape(-1,3)
+    image_points = K.dot(p.T).T
+    points = image_points[:,:-1]/image_points[:,[-1]]
+    points = points.reshape(img.shape[0],img.shape[1],-1)
+    cylinder = cv2.remap(img, (points[:, :, 0]).astype(np.float32), (points[:, :, 1]).astype(np.float32), cv2.INTER_LINEAR)
+    return cylinder
+
 noi = 4 # number of images to be stich
 img1 = cv2.imread('1.jpeg')
 img1 = cv2.resize(img1, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
